f84aa8ace4685ffc4524e8318e9178f5bbac2971
jjkip <jjlangatjose@gmail.com>
2023-08-15 22:52:59 +0300

    Initial Commit

---
+++ UneveRoundedRect/UneveRoundedRectApp.swift
@@ -0,0 +1,17 @@
+//
+//  UneveRoundedRectApp.swift
+//  UneveRoundedRect
+//
+//  Created by Joseph Langat on 15/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct UneveRoundedRectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
